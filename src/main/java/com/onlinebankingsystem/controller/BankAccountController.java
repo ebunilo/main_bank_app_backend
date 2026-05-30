6d90f7ff742364e504f9b8d3419df93a24870365
dammy <kolawoledamilola779@gmail.com>
2026-05-30 15:15:19 +0100

    Updated Domain

--- src/main/java/com/onlinebankingsystem/controller/BankAccountController.java
+++ src/main/java/com/onlinebankingsystem/controller/BankAccountController.java
@@ -20,7 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("api/bank/account")
-@CrossOrigin(origins = "https://bank.cloudwitches.online")
+@CrossOrigin(origins = "https:// bank.themainpod14.online")
 public class BankAccountController {
 
 	@Autowired
--- src/main/java/com/onlinebankingsystem/controller/BankAccountTransactionController.java
+++ src/main/java/com/onlinebankingsystem/controller/BankAccountTransactionController.java
@@ -23,7 +23,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("api/bank/transaction")
-@CrossOrigin(origins = "https://bank.cloudwitches.online")
+@CrossOrigin(origins = "https:// bank.themainpod14.online")
 public class BankAccountTransactionController {
 
 	@Autowired
